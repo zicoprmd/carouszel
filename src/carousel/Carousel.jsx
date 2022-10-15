d796b1a4965ce9e2cf141b56f802f646474e7b65
zicoprmd <zicoprmd@gmail.com>
2022-10-15 22:07:52 +0700

    addedease transition

--- src/carousel/Carousel.jsx
+++ src/carousel/Carousel.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import CarouselItem from './CarouselItem';
 import './Carousel.scss';
 
 const Carousel = ({ slides }) => {
+  const [currentSlide, setcurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setcurrentSlide((currentSlide) =>
+        currentSlide < slides.length - 1 ? currentSlide + 1 : 0
+      );
+    }, 3000);
+    return () => {
+      clearInterval(slideInterval);
+    };
+  }, []);
+
   return (
     <div className="carousel">
-      <div className="carousel-inner">
+      <div
+        className="carousel-inner"
+        style={{ transform: `translateX(${-currentSlide * 100}%)` }}>
         {slides.map((slide, index) => (
           <CarouselItem slide={slide} key={index} />
         ))}

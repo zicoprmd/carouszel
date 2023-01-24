eac68cca82e5b07bd01c1422b4bf1b278b4ac8d1
zicoprmd <zicoprmd@gmail.com>
2023-01-24 13:09:38 +0700

    added static image import meta URL

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import Carousel from './carousel/Carousel';
 
 // import foto assets
+const zemaUrl = new URL('./assets/zem5.JPG', import.meta.url).href;
+const zema1 = new URL('./assets/zem6.JPG', import.meta.url).href;
+const zema2 = new URL('./assets/zem7.JPG', import.meta.url).href;
+const zema3 = new URL('./assets/zem8.JPG', import.meta.url).href;
 
 function App() {
   const slides = [
     'http://localhost:5173/zem1.JPG',
-    'http://localhost:5173/zem6.JPG',
-    'http://localhost:5173/zem7.JPG',
-    'http://localhost:5173/zem8.JPG',
+    'http://localhost:5173/zem2.JPG',
+    'http://localhost:5173/zem3.JPG',
+    'http://localhost:5173/zem4.JPG',
+    zemaUrl,
+    zema1,
+    zema2,
+    zema3,
   ];
 
   return (
